e5533136146ea8b075bfce6ed5f8888d71a6b7bc
ayman <ayman_p@hotmail.fr>
2026-04-02 11:11:04 +0200

    Init: installation des dépendances de base

---
+++ SCR/connexion_mt5.py
@@ -0,0 +1,24 @@
+import MetaTrader5 as mt5
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def connexion():
+    if not mt5.initialize():
+        print(f"Erreur de connexion : {mt5.last_error()}")
+        return False
+
+    print(f"Connecté à MetaTrader5 ")
+    print(f"Version : {mt5.version()}")
+    print(f"Broker : {mt5.account_info().company}")
+    print(f"Balance : {mt5.account_info().balance} €")
+    return True
+
+def deconnexion():
+    mt5.shutdown()
+    print("Déconnecté de MT5")
+
+if __name__ == "__main__":
+    if connexion():
+        deconnexion()
